9f3a09ea9456bdeeb55e9b96d308d29dc14c818f
Muntissa <h3r1zma@gmail.com>
2022-11-22 13:55:31 +0300

    Added lexem classification

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,87 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lexicalAnalysisiTB = new System.Windows.Forms.TextBox();
+            this.buttonLB1 = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.button2 = new System.Windows.Forms.Button();
+            this.tabPageLB1 = new System.Windows.Forms.TabPage();
+            this.lexicalAnalysisLabel = new System.Windows.Forms.Label();
+            this.tabPageLB2 = new System.Windows.Forms.TabPage();
+            this.leteralTB = new System.Windows.Forms.Label();
+            this.buttonGenerealTB = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.buttonFourTB = new System.Windows.Forms.Button();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBoxKeyWord = new System.Windows.Forms.TextBox();
+            this.textBoxLiteral = new System.Windows.Forms.TextBox();
+            this.idTB = new System.Windows.Forms.Label();
+            this.keyWordTB = new System.Windows.Forms.Label();
+            this.separatorTB = new System.Windows.Forms.Label();
+            this.classificationLabel = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
+            this.tabPageLB1.SuspendLayout();
+            this.tabPageLB2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // lexicalAnalysisiTB
             // 
-            this.textBox1.Location = new System.Drawing.Point(6, 6);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(354, 661);
-            this.textBox1.TabIndex = 0;
+            this.lexicalAnalysisiTB.Location = new System.Drawing.Point(6, 42);
+            this.lexicalAnalysisiTB.Multiline = true;
+            this.lexicalAnalysisiTB.Name = "lexicalAnalysisiTB";
+            this.lexicalAnalysisiTB.Size = new System.Drawing.Size(354, 625);
+            this.lexicalAnalysisiTB.TabIndex = 0;
             // 
-            // button1
+            // buttonLB1
             // 
-            this.button1.Location = new System.Drawing.Point(423, 86);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonLB1.Location = new System.Drawing.Point(366, 626);
+            this.buttonLB1.Name = "buttonLB1";
+            this.buttonLB1.Size = new System.Drawing.Size(136, 41);
+            this.buttonLB1.TabIndex = 1;
+            this.buttonLB1.Text = "Start";
+            this.buttonLB1.UseVisualStyleBackColor = true;
+            this.buttonLB1.Click += new System.EventHandler(this.buttonLB1_Click);
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPageLB1);
+            this.tabControl1.Controls.Add(this.tabPageLB2);
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1151, 701);
             this.tabControl1.TabIndex = 2;
             // 
-            // tabPage1
+            // tabPageLB1
             // 
-            this.tabPage1.Controls.Add(this.textBox1);
-            this.tabPage1.Controls.Add(this.button1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 24);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1143, 673);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPageLB1.Controls.Add(this.lexicalAnalysisLabel);
+            this.tabPageLB1.Controls.Add(this.lexicalAnalysisiTB);
+            this.tabPageLB1.Controls.Add(this.buttonLB1);
+            this.tabPageLB1.Location = new System.Drawing.Point(4, 24);
+            this.tabPageLB1.Name = "tabPageLB1";
+            this.tabPageLB1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageLB1.Size = new System.Drawing.Size(1143, 673);
+            this.tabPageLB1.TabIndex = 0;
+            this.tabPageLB1.Text = "LB1";
+            this.tabPageLB1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // lexicalAnalysisLabel
             // 
-            this.tabPage2.Controls.Add(this.textBox6);
-            this.tabPage2.Controls.Add(this.button2);
-            this.tabPage2.Controls.Add(this.textBox5);
-            this.tabPage2.Controls.Add(this.textBox4);
-            this.tabPage2.Controls.Add(this.textBox3);
-            this.tabPage2.Controls.Add(this.textBox2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 24);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1143, 673);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.lexicalAnalysisLabel.AutoSize = true;
+            this.lexicalAnalysisLabel.Location = new System.Drawing.Point(18, 15);
+            this.lexicalAnalysisLabel.Name = "lexicalAnalysisLabel";
+            this.lexicalAnalysisLabel.Size = new System.Drawing.Size(120, 15);
+            this.lexicalAnalysisLabel.TabIndex = 2;
+            this.lexicalAnalysisLabel.Text = "Лексический анализ";
             // 
-            // button2
+            // tabPageLB2
             // 
-            this.button2.Location = new System.Drawing.Point(511, 25);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.tabPageLB2.Controls.Add(this.classificationLabel);
+            this.tabPageLB2.Controls.Add(this.separatorTB);
+            this.tabPageLB2.Controls.Add(this.keyWordTB);
+            this.tabPageLB2.Controls.Add(this.idTB);
+            this.tabPageLB2.Controls.Add(this.leteralTB);
+            this.tabPageLB2.Controls.Add(this.buttonGenerealTB);
+            this.tabPageLB2.Controls.Add(this.textBox6);
+            this.tabPageLB2.Controls.Add(this.buttonFourTB);
+            this.tabPageLB2.Controls.Add(this.textBox5);
+            this.tabPageLB2.Controls.Add(this.textBox4);
+            this.tabPageLB2.Controls.Add(this.textBoxKeyWord);
+            this.tabPageLB2.Controls.Add(this.textBoxLiteral);
+            this.tabPageLB2.Location = new System.Drawing.Point(4, 24);
+            this.tabPageLB2.Name = "tabPageLB2";
+            this.tabPageLB2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageLB2.Size = new System.Drawing.Size(1143, 673);
+            this.tabPageLB2.TabIndex = 1;
+            this.tabPageLB2.Text = "LB2";
+            this.tabPageLB2.UseVisualStyleBackColor = true;
+            // 
+            // leteralTB
+            // 
+            this.leteralTB.AutoSize = true;
+            this.leteralTB.Location = new System.Drawing.Point(6, 7);
+            this.leteralTB.Name = "leteralTB";
+            this.leteralTB.Size = new System.Drawing.Size(62, 15);
+            this.leteralTB.TabIndex = 7;
+            this.leteralTB.Text = "Литералы";
+            // 
+            // buttonGenerealTB
+            // 
+            this.buttonGenerealTB.Location = new System.Drawing.Point(365, 113);
+            this.buttonGenerealTB.Name = "buttonGenerealTB";
+            this.buttonGenerealTB.Size = new System.Drawing.Size(417, 50);
+            this.buttonGenerealTB.TabIndex = 6;
+            this.buttonGenerealTB.Text = "Общий список";
+            this.buttonGenerealTB.UseVisualStyleBackColor = true;
+            this.buttonGenerealTB.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(540, 169);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(76, 474);
+            this.textBox6.TabIndex = 5;
+            // 
+            // buttonFourTB
+            // 
+            this.buttonFourTB.Location = new System.Drawing.Point(365, 59);
+            this.buttonFourTB.Name = "buttonFourTB";
+            this.buttonFourTB.Size = new System.Drawing.Size(417, 48);
+            this.buttonFourTB.TabIndex = 4;
+            this.buttonFourTB.Text = "Распределить по спискам";
+            this.buttonFourTB.UseVisualStyleBackColor = true;
+            this.buttonFourTB.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox5
             // 
@@ -126,29 +176,58 @@
             this.textBox4.Size = new System.Drawing.Size(344, 306);
             this.textBox4.TabIndex = 2;
             // 
-            // textBox3
+            // textBoxKeyWord
             // 
-            this.textBox3.Location = new System.Drawing.Point(788, 25);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(339, 289);
-            this.textBox3.TabIndex = 1;
+            this.textBoxKeyWord.Location = new System.Drawing.Point(788, 25);
+            this.textBoxKeyWord.Multiline = true;
+            this.textBoxKeyWord.Name = "textBoxKeyWord";
+            this.textBoxKeyWord.Size = new System.Drawing.Size(339, 289);
+            this.textBoxKeyWord.TabIndex = 1;
             // 
-            // textBox2
+            // textBoxLiteral
             // 
-            this.textBox2.Location = new System.Drawing.Point(6, 25);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(344, 289);
-            this.textBox2.TabIndex = 0;
+            this.textBoxLiteral.Location = new System.Drawing.Point(6, 25);
+            this.textBoxLiteral.Multiline = true;
+            this.textBoxLiteral.Name = "textBoxLiteral";
+            this.textBoxLiteral.Size = new System.Drawing.Size(344, 289);
+            this.textBoxLiteral.TabIndex = 0;
             // 
-            // textBox6
+            // idTB
             // 
-            this.textBox6.Location = new System.Drawing.Point(372, 169);
-            this.textBox6.Multiline = true;
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(397, 474);
-            this.textBox6.TabIndex = 5;
+            this.idTB.AutoSize = true;
+            this.idTB.Location = new System.Drawing.Point(6, 319);
+            this.idTB.Name = "idTB";
+            this.idTB.Size = new System.Drawing.Size(103, 15);
+            this.idTB.TabIndex = 8;
+            this.idTB.Text = "Идентификаторы";
+            // 
+            // keyWordTB
+            // 
+            this.keyWordTB.AutoSize = true;
+            this.keyWordTB.Location = new System.Drawing.Point(788, 7);
+            this.keyWordTB.Name = "keyWordTB";
+            this.keyWordTB.Size = new System.Drawing.Size(100, 15);
+            this.keyWordTB.TabIndex = 9;
+            this.keyWordTB.Text = "Ключевые слова";
+            // 
+            // separatorTB
+            // 
+            this.separatorTB.AutoSize = true;
+            this.separatorTB.Location = new System.Drawing.Point(788, 319);
+            this.separatorTB.Name = "separatorTB";
+            this.separatorTB.Size = new System.Drawing.Size(76, 15);
+            this.separatorTB.TabIndex = 10;
+            this.separatorTB.Text = "Разделители";
+            // 
+            // classificationLabel
+            // 
+            this.classificationLabel.AutoSize = true;
+            this.classificationLabel.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.classificationLabel.Location = new System.Drawing.Point(426, 10);
+            this.classificationLabel.Name = "classificationLabel";
+            this.classificationLabel.Size = new System.Drawing.Size(302, 37);
+            this.classificationLabel.TabIndex = 11;
+            this.classificationLabel.Text = "Классификация лексем";
             // 
             // Form1
             // 
@@ -159,26 +238,33 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
-            this.tabPage2.ResumeLayout(false);
-            this.tabPage2.PerformLayout();
+            this.tabPageLB1.ResumeLayout(false);
+            this.tabPageLB1.PerformLayout();
+            this.tabPageLB2.ResumeLayout(false);
+            this.tabPageLB2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private TextBox textBox1;
-        private Button button1;
+        private TextBox lexicalAnalysisiTB;
+        private Button buttonLB1;
         private TabControl tabControl1;
-        private TabPage tabPage1;
-        private TabPage tabPage2;
-        private Button button2;
+        private TabPage tabPageLB1;
+        private TabPage tabPageLB2;
+        private Button buttonFourTB;
         private TextBox textBox5;
         private TextBox textBox4;
-        private TextBox textBox3;
-        private TextBox textBox2;
+        private TextBox textBoxKeyWord;
+        private TextBox textBoxLiteral;
         private TextBox textBox6;
+        private Button buttonGenerealTB;
+        private Label lexicalAnalysisLabel;
+        private Label leteralTB;
+        private Label classificationLabel;
+        private Label separatorTB;
+        private Label keyWordTB;
+        private Label idTB;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -8,15 +8,30 @@ namespace SimpleCompilatorClang
         }
         string inputString = "int main() { int k; int sum = 0; for(int i=1; i<=k; i++) {sum = sum + i; } }";
         Variables variables = new Variables(new List<string> { "(", ")", "{", "}", "=", ";" }, new List<string> { "<=" }, new List<string> { "for", "int", "main" });
-        List<string> listLeteral, listKeyWord, listID, listSeparator; 
- 
+        List<string> listLeteral, listKeyWord, listID, listSeparator;
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var firstList = Helper.FirstProcess(inputString, variables);
+            foreach(var word in firstList)
+            {
+                if (listLeteral.Contains(word))
+                    textBox6.Text += "( " + "1" + " : " + listLeteral.IndexOf(word) + " )" + Environment.NewLine;
+                else if (listKeyWord.Contains(word))
+                    textBox6.Text += "( " + "2" + " : " + listKeyWord.IndexOf(word) + " )" + Environment.NewLine;
+                else if (listID.Contains(word))
+                    textBox6.Text += "( " + "3" + " : " + listID.IndexOf(word) + " )" + Environment.NewLine;
+                else if (listSeparator.Contains(word))
+                    textBox6.Text += "( " + "4" + " : " + listSeparator.IndexOf(word) + " )" + Environment.NewLine;
+            }
+        }
+
+        private void buttonLB1_Click(object sender, EventArgs e)
         {
             var groupList = Helper.FirstProcess(inputString, variables);
-            textBox1.Text = "";
+            lexicalAnalysisiTB.Text = "";
             foreach (var item in groupList)
-                textBox1.Text += item + " - " + Helper.GetStringType(item, variables) + Environment.NewLine;
+                lexicalAnalysisiTB.Text += item + " - " + Helper.GetStringType(item, variables) + Environment.NewLine;
         }
         
         private void button2_Click(object sender, EventArgs e)
@@ -27,13 +42,13 @@ namespace SimpleCompilatorClang
                 {
                     listLeteral = list.Where(group => group.Key == "Literal").SelectMany(m => m).Distinct().ToList();
                     foreach (var item in listLeteral)
-                        textBox2.Text += listLeteral.IndexOf(item) + " - " + item + Environment.NewLine;
+                        textBoxLiteral.Text += listLeteral.IndexOf(item) + " - " + item + Environment.NewLine;
                 }
                 else if (group.Key == "KeyWord")
                 {
                     listKeyWord = list.Where(group => group.Key == "KeyWord").SelectMany(m => m).Distinct().ToList();
                     foreach (var item in listKeyWord)
-                        textBox3.Text += listKeyWord.IndexOf(item) + " - " + item + Environment.NewLine;
+                        textBoxKeyWord.Text += listKeyWord.IndexOf(item) + " - " + item + Environment.NewLine;
                 }
                 else if (group.Key == "ID")
                 {
